1e566be27cc22860cec3148fbba4004ba6403c6a
Sil Westerveld <git@silwesterveld.com>
2019-01-23 23:10:43 +0100

    Fix old notation

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,6 +1,6 @@
 describe('Address Book', function() {
   it('should be able to add a contact', function() {
-    var addressBook = new AddressBook(),
+    let addressBook = new AddressBook(),
       thisContact = new Contact()
 
     addressBook.addContact(thisContact)
