1a8d2968c59b2ee14308a5612e275e22269f6065
Sil Westerveld <git@silwesterveld.com>
2019-01-23 23:16:45 +0100

    Remove redundant code by using beforeEach()

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,17 +1,18 @@
 describe('Address Book', function() {
-  it('should be able to add a contact', function() {
-    let addressBook = new AddressBook(),
-      thisContact = new Contact()
+  let addressBook, thisContact
 
+  beforeEach(function() {
+    addressBook = new AddressBook()
+    thisContact = new Contact()
+  })
+
+  it('should be able to add a contact', function() {
     addressBook.addContact(thisContact)
 
     expect(addressBook.getContact(0)).toBe(thisContact)
   })
 
   it('should be able to delete a contact', function() {
-    let addressBook = new AddressBook(),
-      thisContact = new Contact()
-
     addressBook.addContact(thisContact)
     addressBook.deleteContact(0)
 
